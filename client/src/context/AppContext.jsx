2b74fb598cade2e8bf1e62615d1a350221e5ea11
Biswash Khanal <kbiswash47@gmail.com>
2025-06-09 18:16:45 +0545

    Initial Commit - frontend setup with vite@react

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+	const navigate = useNavigate();
+	const [user, setUser] = useState(null);
+	const [isSeller, setIsSeller] = useState(null);
+
+	const value = { navigate, user, setUser, setIsSeller, isSeller };
+
+	return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+	return useContext(AppContext);
+};
