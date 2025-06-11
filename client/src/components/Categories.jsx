f5300aa451cacf725d0747ee465241ccf4f7c439
Biswash Khanal <kbiswash47@gmail.com>
2025-06-11 20:14:56 +0545

    Added Best seller component, ProductCard Component and some tweaks

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -10,7 +10,7 @@ const Categories = () => {
 	return (
 		<div className="mt-16">
 			<p className="text-2xl md:text-3xl font-medium">Categories</p>
-			<div className="grid gap-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 ">
+			<div className="grid gap-6 grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7">
 				{categories.map((category, index) => (
 					<div
 						key={index}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,15 +1,70 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+	const currency = import.meta.VITE_CURRENCY;
 	const navigate = useNavigate();
 	const [user, setUser] = useState(null);
 	const [isSeller, setIsSeller] = useState(false);
 	const [showUserLogin, setShowUserLogin] = useState(false);
+	const [products, setProducts] = useState([]);
+	const [cartItems, setCartItems] = useState([]);
 
-	const value = { navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin };
+//Fetch all products function
+	const fetchProducts = async()=>{
+		setProducts(dummyProducts)
+	}
+
+	//Add products to cart function
+	const addToCart=(itemID)=>{
+		console.log("test3")
+		toast.success("Added To Cart!")
+		let cartData = structuredClone(cartItems);
+
+		if(cartData[itemID]){
+			cartData[itemID] +=1;
+		}
+		else{
+			cartData[itemID] -=1;
+		}
+		setCartItems(cartData);
+		toast.success("Added To Cart!")
+	}
+
+		//Update Cart Quantity
+	const updateCartItem=(itemID, quantity)=>{
+		let cartData = structuredClone(cartItems);
+		cartData[itemID] = quantity;
+		setCartItems(cartData)
+		toast.success("Cart Updated")
+	}
+
+
+		//Remove products to cart function
+	const removeFromCart=(itemID)=>{
+		let cartData = structuredClone(cartItems);
+
+		if(cartData[itemID]){
+			cartData[itemID] -=1;
+			if(cartData[itemID]===0){
+				delete cartData[itemID]
+			}
+		}
+
+		toast.success("Removed From Cart!")
+		setCartItems(cartData);
+	}
+
+
+
+	useEffect(()=>{
+		fetchProducts()
+	}, [])
+	const value = { navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems };
 
 	return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
