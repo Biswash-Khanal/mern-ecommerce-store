aa314be4f133baa6e8b6a90c4cdefd963b4c3616
Biswash Khanal <kbiswash47@gmail.com>
2025-06-12 08:06:12 +0545

    Added Login Component, All Products page, and complted the product card component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const NavBar = () => {
 	const [open, setOpen] = React.useState(false);
-	const { user, setUser, setShowUserLogin, navigate } = useAppContext();
+	const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery } = useAppContext();
 
 	const logout = async () => {
 		setUser(null);
 		navigate("/");
 	};
+
+	useEffect(()=>{
+		if(searchQuery.length>0){
+			navigate("/products")
+		}
+	}, [searchQuery])
 
 	return (
 		<nav className="sticky top-0 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white transition-all z-100">
@@ -33,6 +39,7 @@ const NavBar = () => {
 						className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
 						type="text"
 						placeholder="Search products"
+						onChange={(e)=>setSearchQuery(e.target.value)}
 					/>
 					<img
 						src={assets.search_icon}
