32bdb66b88202c2620f4f7ffe23b5256b531f063
Biswash Khanal <kbiswash47@gmail.com>
2025-06-14 17:26:22 +0545

    Created SellerLogin component and page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
 const App = () => {
 
 const isSellerPath = useLocation().pathname.includes("seller");
@@ -32,6 +33,7 @@ const {showUserLogin} = useAppContext();
           <Route path='/cart' element={<Cart/>} />
           <Route path='/add-address' element={<AddAddress/>} />
           <Route path='/my-orders' element={<MyOrders/>} />
+          <Route path='/seller' element={<SellerLogin/>} />
 
         </Routes>
       </div>
